adf7eb95353292034244d57da8fec04b711cee5e
quintin156 <99997788+quintin156@users.noreply.github.com>
2022-11-06 23:37:30 -0500

    Initial commit

---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,13 @@
+function HangmanDrawing() {
+  return (
+    <div style={{ position: "relative" }}>
+      <div
+        style={{ height: "10px", width: "250px", background: "black" }}
+      >
+        
+      </div>
+    </div>
+  );
+}
+
+export default HangmanDrawing;
---
+++ src/components/HangmanWord.tsx
@@ -0,0 +1,9 @@
+
+
+function HangmanWord() {
+  return (
+    <div>HangmanWord</div>
+  )
+}
+
+export default HangmanWord
